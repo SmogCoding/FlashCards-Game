edad2fde80abf80439680cc2129efbf854c1a2b5
SmogCoding <smogcoding2030@gmail.com>
2023-07-23 12:16:02 +0100

    Added the ability for the user to add their words to the flashcard data sturcture

--- main.py
+++ main.py
@@ -11,10 +11,53 @@ def get_num_words():
         if not number_of_words.isdigit():
             print("Please enter a valid number")
         else:
+            number_of_words = int(number_of_words)
             break
 
     return number_of_words
 
 
+def get_native_word():
+    while True:
+        native_word = input(
+            "Please enter the native word for the flashcard: \n").lower()
+
+        if native_word.strip():
+            return native_word
+        else:
+            print("Please enter a valid word")
+
+
+def get_non_native_word():
+    while True:
+        non_native_word = input(
+            "Please enter the non-native language word: \n").lower()
+
+        if non_native_word.strip():
+            return non_native_word
+        else:
+            print("Please enter a valid word")
+
+
 # Get the number of words to guess
 num_of_words = get_num_words()
+
+
+def get_flashcards_words(num_words):
+    '''
+    Function gets the native and non-native languages from the user to be stored in the
+    flashcard data structure.
+    '''
+    for _ in range(num_words):
+        native_language = get_native_word()
+        non_native_language = get_non_native_word()
+
+        # Checks to see if the native language is already the key, and makes sure there is not duplicate words
+        if native_language in flashcards:
+            print("Word already in the selection")
+        else:
+            flashcards[native_language] = non_native_language
+
+
+get_flashcards_words(num_of_words)
+print(flashcards)
