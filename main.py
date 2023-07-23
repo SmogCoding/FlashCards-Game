85b107eb7d742c770977700ab68e1b82142e9572
SmogCoding <smogcoding2030@gmail.com>
2023-07-23 11:42:53 +0100

    Implemented asking how many words they want to guess function

--- main.py
+++ main.py
@@ -1,1 +1,20 @@
-print("Hello")
+flashcards = {}
+
+
+def get_num_words():
+    '''
+    Function asks the user to enter how many words they want to guess
+    '''
+    while True:
+        number_of_words = input("How many words do you want to guess? \n")
+
+        if not number_of_words.isdigit():
+            print("Please enter a valid number")
+        else:
+            break
+
+    return number_of_words
+
+
+# Get the number of words to guess
+num_of_words = get_num_words()
